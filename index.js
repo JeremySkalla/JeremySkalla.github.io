ae1a33b08e04331300f2a030c97b4405f1456169
JeremySkalla <77216636+JeremySkalla@users.noreply.github.com>
2022-02-28 19:28:09 -0600

    Mobile Support Added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var currentActive = "main";
+var mainNewTop = "0%";
+var linkNewTextSize = "1.5em";
 let jeremyskalla = document.getElementById("name");
 let linkDiv = document.getElementById("link-container");
 let secondLinkDiv = document.getElementById('second-link-div');
 let about = document.getElementById('about');
 let projects = document.getElementById('projects');
 let work = document.getElementById('workexp');
-let content = document.getElementById('content')
+let content = document.getElementById('content');
 
 function modeChange() {
     if (jeremyskalla.style.color == "rgb(33, 33, 33)") {
@@ -42,11 +44,11 @@ function moveUp(newactive) {
     restoreToPrev();
     currentActive = newactive;
 
-    jeremyskalla.style.top = "0%";
-    jeremyskalla.style.fontSize="4em";
+    jeremyskalla.style.top = mainNewTop;
+    jeremyskalla.style.fontSize = "4em";
 
     linkDiv.style.top = "5%";
-    linkDiv.style.fontSize="1.5em";
+    linkDiv.style.fontSize = linkNewTextSize;
     secondLinkDiv.style.margin = "0 5.75%";
 }
 
@@ -55,7 +57,7 @@ function activateAbout() {
     about.style.cursor = "default";
     document.getElementById('about-underline').classList.remove("underline");
     moveUp("about");
-    setTimeout(function () { content.hidden = false; }, 1000);
+    setTimeout(function () { content.hidden = false; }, 1000); // Move this to moveUp once content done for all
 }
 
 function activateProjects() {
@@ -75,3 +77,12 @@ function activateWork() {
 about.addEventListener('click', activateAbout);
 projects.addEventListener('click', activateProjects);
 work.addEventListener('click', activateWork);
+console.log(document.body.style.width)
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    content.style.width = "90%"; // Adjust width for mobile devices
+    content.style.fontSize = "2.5em";
+    linkDiv.style.top = "40%";
+    mainNewTop = "2.5%";
+    linkNewTextSize = "2em";
+}
