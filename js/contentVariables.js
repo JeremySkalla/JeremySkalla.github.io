288e4d49505082492afa26898a929e9f4d57761a
JeremySkalla <77216636+JeremySkalla@users.noreply.github.com>
2022-03-02 01:05:50 -0600

    Website done except for Paragraphs

--- js/contentVariables.js
+++ js/contentVariables.js
@@ -1,6 +1,13 @@
 let aboutHeader = "Who Am I?";
-let projectsHeader1 = "<a href=\"https://github.com/JeremySkalla/pysmashgg\">Pysmashgg</a>";
+
 let workHeader1 = "ActiFi";
+let workHeader2 = "temp1"
+let workHeader3 = "temp2"
+
+let firstButtons = `<button class="bottom" onclick="nextPage()">Next</button>`;
+let middleButtons = `<button class="bottom" onclick="prevPage()">Prev</button>
+<button class="bottom" onclick="nextPage()">Next</button>`;
+let lastButtons = `<button class="bottom" onclick="prevPage()">Prev</button>`;
 
 let aboutContent = `Hello! I'm Jeremy Skalla, and I'm a 21 year old Computer Science Major at the <span style="color:#7a0019">University</span> of 
 <span style="color:#ffcc33">Minnesota</span>. I have experience in <span style="color:#306998">Python</span>, 
@@ -14,7 +21,7 @@ and my favorite albums of all time are <span style="color:#313733">Rodeo</span> 
 <span style="color:#ce1e35">My Beautiful Dark Twisted Fantasy</span> by Kanye West, and 
 <span style="color:#dcd1b3">What's the Story Morning Glory</span> by Oasis. Poker is one of my favorite past-times,
 and I love trivia. Lastly, I'm super heavily involved in esports, as you can see by my projects, 
-especially Super Smash Brothers Melee.`
+especially Super Smash Brothers Melee.`;
 
 let projectsContent1 = `<a href="https://github.com/JeremySkalla/pysmashgg" style="color:#6E5494">Pysmashgg</a> is a simple wrapper for 
 <a href="https://smash.gg/" style="color:#cb333b">smash.gg's</a> 
@@ -27,8 +34,13 @@ or poorly written/documented ones. So, pysmashgg was born.<br><br>
 This was written all in <span style="color:#306998">Python</span>. I organized the files in 
 the github, as well as created detailed documentation about how to use the wrapper. One of my biggest goals 
 with this project was to help other newer coders understand how the API works. It was pretty cool getting 
-people telling me that my program helped them. I've worked on it intermittently since the start, 
-as I've needed it for another one of my projects, JUMS.`
+people telling me that my program helped them. I continue to work on it,
+as I've needed it for another one of my projects, JUMS.`;
+
+let projectsHeaderArr = ["<a href=\"https://github.com/JeremySkalla/pysmashgg\">Pysmashgg</a>", 
+                        "This Website", "JUMS", "Discord Queue Bot"];
+let projectsH2Arr = ["December 2020-Current", "December 2021-Current", "February 2022-Current", "January 2021-March 2021"]
+let projectsContentArr = [projectsContent1, "TODO", "TODO", "TODO"];
 
 let workContent1 = `I worked at <span style="color:#216093">ActiFi</span> as a summer intern between April 2021-September 2021. 
 During this time, I had many roles. I tested and documented their SuccessPro software for implementation 
@@ -39,3 +51,7 @@ This was also my first time getting experience in Salesforce, where I created an
 On the less technical side of things, I attended meetings with the CEO and other members of the company, 
 discussing plans for growth in the future. During these meetings, I took notes and created a glossary, both 
 for the use of new employees. This was put into action when they hired a new member to the Sales team that summer.`
+
+let workHeaderArr = ["ActiFi", "Grinning Goat Gaming", "Culver's"];
+let workH2Arr = ["April-August 2021", "November 2015-October 2018", "April-September 2017"];
+let workContentArr = [workContent1, "TODO", "TODO"];
--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 var currentActive = "main";
-var mainNewTop = "0%";
+var mainNewTop = "1%";
 var linkNewTextSize = "1.5em";
 var movedUpOnce = false;
+var projectsPage = 0;
+var workPage = 0;
+var numPPages = projectsContentArr.length-1;
+var numWPages = workContentArr.length-1;
 
 let jeremyskalla = document.getElementById("name");
 let linkDiv = document.getElementById("link-container");
@@ -12,6 +16,8 @@ let work = document.getElementById('workexp');
 let content = document.getElementById('content');
 let contentText = document.getElementById('content-text');
 let contentHeader = document.getElementById('content-header');
+let contentSubHeader = document.getElementById('content-subheader');
+let buttons = document.getElementById('bottom-buttons');
 
 function restoreToPrev() {
     content.hidden = true;
@@ -33,7 +39,6 @@ function restoreToPrev() {
 }
 
 function moveUp(newactive) {
-    console.log(movedUpOnce)
     if (!movedUpOnce) { // Changing page
         setTimeout(function () { content.hidden = false; }, 1000); // Move this to moveUp once content done for all
         movedUpOnce = true;
@@ -52,7 +57,55 @@ function moveUp(newactive) {
     secondLinkDiv.style.margin = "0 5.75%";
 }
 
-// These are the same basically so I'm gonna comment only on the first
+function pageButtons(pageCount) { // Retrieves buttons for both projects and work
+    if (pageCount == 0) {
+        return firstButtons; 
+    }
+    else if (currentActive == "projects" && pageCount == numPPages) {
+        return lastButtons; 
+    }
+    else if (currentActive == "work" && pageCount == numWPages) {
+        return lastButtons; 
+    }
+    else { 
+        return middleButtons; 
+    }
+}
+
+function nextPage() {
+    if (currentActive == "projects") {
+        projectsPage += 1;
+        contentHeader.innerHTML = projectsHeaderArr[projectsPage];
+        contentSubHeader.innerHTML = projectsH2Arr[projectsPage];
+        contentText.innerHTML = projectsContentArr[projectsPage];
+        buttons.innerHTML = pageButtons(projectsPage);
+    }
+    else if (currentActive == "work") {
+        workPage += 1;
+        contentHeader.innerHTML = workHeaderArr[workPage];
+        contentSubHeader.innerHTML = projectsH2Arr[workPage];
+        contentText.innerHTML = workContentArr[workPage];
+        buttons.innerHTML = pageButtons(workPage);
+    }
+}
+
+function prevPage() {
+    if (currentActive == "projects") {
+        projectsPage -= 1;
+        contentHeader.innerHTML = projectsHeaderArr[projectsPage];
+        contentSubHeader.innerHTML = projectsH2Arr[projectsPage];
+        contentText.innerHTML = projectsContentArr[projectsPage];
+        buttons.innerHTML = pageButtons(projectsPage);
+    }
+    else if (currentActive == "work") {
+        workPage -= 1;
+        contentHeader.innerHTML = workHeaderArr[workPage];
+        contentSubHeader.innerHTML = projectsH2Arr[workPage];
+        contentText.innerHTML = workContentArr[workPage];
+        buttons.innerHTML = pageButtons(workPage);
+    }
+}
+
 function activateAbout() {
     about.style.color = "#42A5F5";
     about.removeEventListener('click', activateAbout); // Removes ability to click
@@ -60,27 +113,34 @@ function activateAbout() {
     moveUp("about");
     contentText.innerHTML = aboutContent;
     contentHeader.innerHTML = aboutHeader;
+    contentSubHeader.innerHTML = "";
+    buttons.innerHTML = "";
 }
 
 function activateProjects() {
     projects.style.color = "#42A5F5";
     projects.removeEventListener('click', activateProjects); // Removes ability to click
     document.getElementById('projects-underline').classList.remove("underline");
-    moveUp("projects");
     contentText.innerHTML = projectsContent1;
-    contentHeader.innerHTML = projectsHeader1;
+    contentHeader.innerHTML = projectsHeaderArr[0];
+    contentSubHeader.innerHTML = projectsH2Arr[0];
+    buttons.innerHTML = firstButtons;
+    projectsPage = 0;
+    moveUp("projects");
 }
 
 function activateWork() {
     work.style.color = "#42A5F5";
     work.removeEventListener('click', activateWork); // Removes ability to click
     document.getElementById('work-underline').classList.remove("underline");
-    moveUp("work");
     contentText.innerHTML = workContent1;
-    contentHeader.innerHTML = workHeader1;
+    contentHeader.innerHTML = workHeaderArr[0];
+    contentSubHeader.innerHTML = workH2Arr[0];
+    buttons.innerHTML = firstButtons;
+    workPage = 0;
+    moveUp("work");
 }
 
 about.addEventListener('click', activateAbout);
 projects.addEventListener('click', activateProjects);
 work.addEventListener('click', activateWork);
-console.log(document.body.style.width);
